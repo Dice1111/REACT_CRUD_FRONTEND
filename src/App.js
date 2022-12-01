88c8f30c6765c6c7ad460e1006e4f1daf3d1327f
Dice1111 <kingdice1111@gmail.com>
2022-12-01 14:46:41 +0800

    list_init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from './components/Header';
+import ListEmployee from './components/ListEmployee';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='container'>
+        <Router>
+        <Routes>
+          <Route index element = {<ListEmployee/>}></Route>
+        </Routes>
+        </Router>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/listEmployee.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import Service from '../services/Service';
+
+export default class ListEmployee extends Component {
+  render() {
+    return (
+      <div style={{color: "red"}}>listEmployee</div>
+    )
+  }
+}
